77e09248e076f543178bf738d00a89ee5098f160
Hayden Baker <hbaker@outlook.com.au>
2023-12-12 15:14:26 +1100

    LOC Rewrite: Day 3 <100

--- C++/src/day_3.cpp
+++ C++/src/day_3.cpp
@@ -4,127 +4,96 @@
 using namespace std;
 using namespace aoc23;
 
-size_t task_one_filter(const string &line, const char *, const size_t &offset);
-size_t task_two_filter(const string &line, const char *, const size_t &offset);
-vector<string> parse_schematic(std::vector<std::string> &lines, char *,
-                               size_t (*find_next_symbol)(const string &, const char *, const size_t &),
-                               bool ignore_duplicates, map<pair<size_t, size_t>, vector<string>> &symbol_part_map);
-void eval_symbol(const size_t x, const size_t y, const size_t row_width, const size_t column_length, 
-    std::vector<std::string> lines, vector<string> &part_numbers, vector<vector<size_t>> &aux, bool ignore_duplicates,
-    map<pair<size_t, size_t>, vector<string>> &symbol_part_map);
+namespace day_3
+{
+    long long task_1(map<pair<size_t, size_t>, vector<int>>);
+    long long task_2(map<pair<size_t, size_t>, vector<int>>, vector<string>);
+
+} // namespace day_3
+
+map<pair<size_t, size_t>, vector<int>> get_part_numbers(vector<string>, bool);
+pair<int, int> get_number_from_digit(string, size_t, size_t, vector<pair<size_t, size_t>> &);
 
 pair<long long, long long> aoc23::day_3()
 {
     std::vector<std::string> lines = read_file("resources/input_3.txt", true);
-    // Strip newline
-    for (size_t i = 0; i < lines.size(); i++)
-    {
-        lines[i] = lines[i].erase(lines[i].find_last_not_of("\r\n") + 1, lines[i].size());
-    }
 
     // Task 1
-    char *non_symbols = "0123456789.";
-    map<pair<size_t, size_t>, vector<string>> symbol_part_map_t1;
-    vector<string> part_numbers = parse_schematic(lines, non_symbols, &task_one_filter, true, symbol_part_map_t1);
-    int total = 0;
-    for (string s: part_numbers)
-    {
-        total += stoi(s);
-    }
+    map<pair<size_t, size_t>, vector<int>> parts_1 = get_part_numbers(lines, true);
+    int total = day_3::task_1(parts_1);
 
     // Task 2
-    char* only_asterisk = "*";
-    map<pair<size_t, size_t>, vector<string>> symbol_part_map_t2;
-    vector<string> t2 = parse_schematic(lines, only_asterisk, &task_two_filter, false, symbol_part_map_t2);
-    int total2 = 0;
-    for (const auto& pair : symbol_part_map_t2) {
-        if (pair.second.size() == 2)
-        {
-            total2 += stoi(pair.second[0]) * stoi(pair.second[1]);
-        }
-    }
+    map<pair<size_t, size_t>, vector<int>> parts_2 = get_part_numbers(lines, false);
+    int total2 = day_3::task_2(parts_2, lines);
 
-    return { total, total2 };
+    return {total, total2};
 }
 
-vector<string> parse_schematic(std::vector<std::string> &lines, char *symbol_set,
-                               size_t (*find_next_symbol)(const string &, const char *, const size_t &),
-                               bool ignore_duplicates, map<pair<size_t, size_t>, vector<string>> &symbol_part_map)
+long long day_3::task_1(map<pair<size_t, size_t>, vector<int>> parts)
 {
-    vector<string> part_numbers;
-
-    size_t x;
-    size_t row_width = lines[0].size();
-    size_t column_length = lines.size();
-
-    vector<vector<size_t>> aux(lines.size());
-    for (size_t y = 0; y < aux.size(); y++)
+    return accumulate(parts.begin(), parts.end(), 0LL, [](long long acc, pair<pair<size_t, size_t>, vector<int>> kv)
     {
-        aux[y] = vector<size_t>(row_width);
-    }
+        return acc + accumulate(kv.second.begin(), kv.second.end(), 0);
+    });
+}
 
-    for (size_t y = 0; y < column_length; y++)
+long long day_3::task_2(map<pair<size_t, size_t>, vector<int>> parts, vector<string> lines)
+{
+    return accumulate(parts.begin(), parts.end(), 0LL, [lines](long long acc, pair<pair<size_t, size_t>, vector<int>> kv)
     {
-        std::string line = lines[y];
-        x = find_next_symbol(line, symbol_set, 0);
-        for (x; x != string::npos; x = find_next_symbol(line, symbol_set, x + 1))
-        {
-            eval_symbol(x, y, row_width, column_length, lines, part_numbers, aux, ignore_duplicates, symbol_part_map);
-        }
-    }
-
-    return part_numbers;
+        return acc + ((lines[kv.first.second][kv.first.first] == '*' && kv.second.size() == 2) ? kv.second[0]*kv.second[1] : 0);
+    });
 }
 
-size_t task_one_filter(const string &line, const char *symbol_set, const size_t &offset)
+map<pair<size_t, size_t>, vector<int>> get_part_numbers(vector<string> lines, bool ignore_duplicates)
 {
-    return line.find_first_not_of(symbol_set, offset);
-}
-
-size_t task_two_filter(const string &line, const char *symbol_set, const size_t &offset)
-{
-    return line.find_first_of(symbol_set, offset);
-}
-
-void eval_symbol(const size_t x, const size_t y, const size_t row_width, const size_t column_length, 
-    std::vector<std::string> lines, vector<string> &part_numbers, vector<vector<size_t>> &aux, bool ignore_duplicates,
-    map<pair<size_t, size_t>, vector<string>> &symbol_part_map)
-{
-    size_t pos_x = max(0, int(x) - 1);
-    size_t pos_y = max(0, int(y) - 1);
-    size_t max_x = min(int(x) + 1, int(row_width) - 1);
-    size_t max_y = min(int(y) + 1, int(row_width) - 1);
-    string digits = "0123456789";
-
-    while (pos_y <= max_y)
+    map<pair<size_t, size_t>, vector<int>> parts;
+    vector<pair<size_t, size_t>> visited;
+    for (int y = 0; y < lines.size(); ++y)
     {
-        while (pos_x <= max_x)
+        string line = lines[y];
+        for (int i = line.find_first_not_of("0123456789.", 0); i != string::npos; i = line.find_first_not_of("0123456789.", i + 1))
         {
-            if (aux[pos_y][pos_x] == 0 || !ignore_duplicates)
+            for (int z = max(0, y - 1); z <= min((int)lines.size() - 1, y + 1); ++z)
             {
-                string line = lines[pos_y];
-                if (digits.find(line[pos_x]) != string::npos) // is digit
+                int x = max(0, i - 1);
+                while (x <= min((int)line.size() - 1, i + 1))
                 {
-                    size_t i_start = pos_x;
-                    while (i_start - 1 >= 0 && digits.find(line[i_start-1]) != string::npos)
+                    pair<size_t, size_t> coord = {x, z};
+                    if ((find(visited.begin(), visited.end(), coord) == visited.end() || !ignore_duplicates) && string(1, lines[z][x]).find_first_of("0123456789") != string::npos)
                     {
-                        i_start--;
-                        aux[pos_y][i_start] = 1;
+                        pair<int, int> to_add = get_number_from_digit(lines[z], x, z, visited);
+                        parts[{i, y}].push_back(stoi(lines[z].substr(to_add.first, to_add.second - to_add.first + 1)));
+                        x = to_add.second + 1;
                     }
-                    size_t i_end = pos_x;
-                    while (i_end + 1 < line.size() && digits.find(line[i_end+1]) != string::npos)
+                    else
                     {
-                        i_end++;
-                        aux[pos_y][i_end] = 1;
+                        ++x;
                     }
-                    part_numbers.push_back(line.substr(i_start, i_end - i_start + 1));
-                    symbol_part_map[make_pair(x, y)].push_back(line.substr(i_start, i_end - i_start + 1));
-                    pos_x = i_end;
                 }
             }
-            pos_x++;
         }
-        pos_x = max(0, int(x) - 1);
-        pos_y++;
     }
+
+    return parts;
+}
+
+pair<int, int> get_number_from_digit(string line, size_t x, size_t y, vector<pair<size_t, size_t>> &visited)
+{
+    // scan left
+    int num_start = x;
+    int num_end = x;
+    visited.push_back({x, y});
+    while (num_start - 1 >= 0 && string(1, line[num_start - 1]).find_first_of("0123456789") != string::npos)
+    {
+        num_start--;
+        visited.push_back({num_start, y});
+    }
+    while (num_end + 1 < line.size() && string(1, line[num_end + 1]).find_first_of("0123456789") != string::npos)
+    {
+        num_end++;
+        visited.push_back({num_end, y});
+    }
+
+    return {num_start, num_end};
 }
